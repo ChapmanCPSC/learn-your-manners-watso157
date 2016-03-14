3af1ef452fed37c7373357fd33f67f8e1bea6e43
Chris Watson <watso157@mail.chapman.edu>
2016-03-14 16:50:42 -0700

    Assignment 3 First Commit

--- HelloName/HelloName/ViewController.swift
+++ HelloName/HelloName/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var lblHelloName: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btnGoTouchedUpInside(sender: AnyObject) {
+        
+        let name = self.tfName.text
+        self.lblHelloName.text = "Hello \(name!)"
+        
+    }
+    
+    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+        print("Hello")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
